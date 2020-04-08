c790cba75c49514748f8686a7bc8198d125c9e10
Ahmed Ihsan Tawfeeq <ahmed_ihsan_tawfeek@yahoo.com>
2020-04-08 18:56:01 +0300

    Added idea layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spnj/widgets/screen.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:like_button/like_button.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -35,7 +36,9 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Screen(
       backgroundColor: Color(0xFFF2E368),
-      children: <Widget>[],
+      children: <Widget>[
+        Idea(),
+      ],
       bottomNavigationBar: CurvedNavigationBar(
         key: _bottomNavigationKey,
         backgroundColor: Color(0xFFF2E368),
@@ -50,6 +53,110 @@ class _HomeScreenState extends State<HomeScreen> {
         onTap: (index) {
           setState(() => _page = index);
         },
+      ),
+    );
+  }
+}
+
+class Idea extends StatelessWidget {
+  const Idea({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(18),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 149,
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(25),
+                child: Text(
+                  'Snakes probably think garden hoses are statues of famous snakes',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(25, 0, 25, 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    LikeButton(
+                      isLiked: false,
+                      size: 30,
+                      circleColor: CircleColor(
+                        start: Colors.redAccent[50],
+                        end: Colors.redAccent[200],
+                      ),
+                      bubblesColor: BubblesColor(
+                        dotPrimaryColor: Colors.redAccent[100],
+                        dotSecondaryColor: Colors.redAccent[300],
+                      ),
+                      likeBuilder: (bool isLiked) {
+                        return Icon(
+                          Icons.favorite,
+                          color: isLiked ? Colors.redAccent : Colors.grey,
+                          size: 30,
+                        );
+                      },
+                      likeCount: 0,
+                      countBuilder: (int count, bool isLiked, String text) {
+                        var color = isLiked ? Colors.redAccent : Colors.grey;
+                        Widget result = Text(
+                          text,
+                          style: TextStyle(color: color),
+                        );
+                        return result;
+                      },
+                    ),
+                    LikeButton(
+                      isLiked: false,
+                      size: 30,
+                      circleColor: CircleColor(
+                        start: Colors.blueGrey,
+                        end: Colors.blueGrey,
+                      ),
+                      bubblesColor: BubblesColor(
+                        dotPrimaryColor: Colors.black,
+                        dotSecondaryColor: Colors.blueGrey,
+                      ),
+                      likeBuilder: (bool isLiked) {
+                        return Icon(
+                          Icons.delete,
+                          color: isLiked ? Colors.black : Colors.grey,
+                          size: 30,
+                        );
+                      },
+                      likeCount: 0,
+                      countBuilder: (int count, bool isLiked, String text) {
+                        var color = isLiked ? Colors.black : Colors.grey;
+                        Widget result = Text(
+                          text,
+                          style: TextStyle(color: color),
+                        );
+                        return result;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
